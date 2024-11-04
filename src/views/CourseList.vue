4ba52ef9b09241aa4f82c6340389efb5ca0bdb32
ChideraN <nwanorohfavoured@gmail.com>
2024-11-04 10:47:28 +0100

    Your commit message here

--- src/views/CourseList.vue
+++ src/views/CourseList.vue
@@ -20,6 +20,7 @@
             </p>
             <button
               class="bg-black text-white py-3 px-6 rounded-lg shadow-lg hover:bg-gray-800 transition duration-200"
+              @click="getStarted"
             >
               Get Started
             </button>
@@ -79,7 +80,7 @@
 
     <!-- about ends -->
 
-    <!--  -->
+    <!-- features section -->
     <section class="bg-[#f8f9f8] p-8 md:p-16 lg:p-26">
       <div class="container mx-auto my-10 md:my-16 px-4 md:px-6 lg:px-16">
         <div>
@@ -150,8 +151,6 @@
       </div>
     </section>
 
-    <!-- ends -->
-
     <!-- courses starts -->
     <section
       id="courses"
@@ -179,43 +178,333 @@
 </template>
 
 <script>
-// import BaseIcon from "@/components/BaseIcon.vue";
 import AcademyCard from "../components/AcademyCard.vue";
-import AcademyService from "../services/AcademyService";
 
 export default {
   name: "CourseList",
   components: {
     AcademyCard,
-    // BaseIcon,
   },
   data() {
     return {
-      courses: [],
+      courses: [
+        {
+          id: "1",
+          title: "Frontend Development",
+          subtitle:
+            "Crafting designs that enhance user interaction, deliver intuitive experiences, and adapt seamlessly across devices.",
+          monthone: 120000,
+          monthtwo: 160000,
+          monththree: 220000,
+          instructor: "Olamide",
+          lessons: "4",
+          image: "course-img1.jpg",
+          durationone: "3 months",
+          durationtwo: "6 months",
+          durationthree: "9 months",
+          lessonone: "7",
+          lessontwo: "24",
+          lessonthree: "32",
+          description:
+            "This course covers the essentials of building responsive and interactive web interfaces using HTML, CSS, and JavaScript. Students will learn about user experience (UX) design principles and how to implement modern frameworks such as React or Vue.js. The curriculum includes hands-on projects that simulate real-world scenarios, allowing students to apply their skills in creating dynamic web applications.",
+          targetAudience: [
+            {
+              group: "Beginners in Web Development",
+              description:
+                "Individuals with little to no prior experience in coding or web design who want to start their journey in building websites and web applications.",
+            },
+            {
+              group: "Aspiring Web Designers",
+              description:
+                "Creative professionals looking to enhance their skills in designing user interfaces (UIs) and user experiences (UX). This course will help them understand how to turn their designs into functional web applications.",
+            },
+            {
+              group: "Current Developers Seeking to Upskill",
+              description:
+                "Developers who have experience in backend development or other programming areas but want to broaden their skill set by learning frontend technologies like HTML, CSS, and JavaScript.",
+            },
+            {
+              group: "Students in Computer Science or Related Fields",
+              description:
+                "College or university students pursuing degrees in computer science, information technology, or digital media who want practical skills to complement their academic learning.",
+            },
+            {
+              group: "Professionals in Digital Marketing or Product Management",
+              description:
+                "Individuals working in digital marketing, product management, or project management who need to understand frontend development concepts to collaborate effectively with development teams.",
+            },
+            {
+              group: "Entrepreneurs and Business Owners",
+              description:
+                "Business owners or entrepreneurs who want to create and manage their websites or web applications without relying solely on external developers. This knowledge empowers them to make informed decisions about their web presence.",
+            },
+            {
+              group: "Career Changers",
+              description:
+                "Professionals from non-technical backgrounds looking to transition into technology and web development. This course provides foundational knowledge and practical skills to facilitate that change.",
+            },
+          ],
+        },
+        {
+          id: "2",
+          title: "Backend Development",
+          subtitle:
+            "Building robust systems with efficient data handling, secure user authentication, and seamless integrations for performance.",
+          monthone: 500,
+          monthtwo: 500,
+          monththree: 500,
+          instructor: "Jane Doe",
+          lessons: "5",
+          image: "course-img2.jpg",
+          durationone: "3 months",
+          durationtwo: "6 months",
+          durationthree: "9 months",
+          lessonone: "10",
+          lessontwo: "20",
+          lessonthree: "30",
+          description:
+            "This course provides an in-depth understanding of backend development, focusing on server-side programming, database management, and API design. Students will learn to build and maintain the technology that powers the client-side of applications, ensuring data integrity and security while delivering high performance. The curriculum includes practical exercises that prepare students for real-world backend development challenges.",
+          targetAudience: [
+            {
+              group: "Beginners in Programming",
+              description:
+                "Individuals who have minimal programming experience but are eager to learn the fundamentals of server-side development.",
+            },
+            {
+              group: "Aspiring Backend Developers",
+              description:
+                "Those who wish to specialize in backend development and understand the principles of building and managing server-side applications.",
+            },
+            {
+              group: "Developers Seeking to Expand Skills",
+              description:
+                "Frontend developers or those in other programming roles looking to gain knowledge of backend technologies and frameworks.",
+            },
+            {
+              group: "Computer Science Students",
+              description:
+                "Students pursuing degrees in computer science or software engineering who want to enhance their understanding of backend architecture and design.",
+            },
+            {
+              group: "Tech Professionals",
+              description:
+                "Individuals working in IT or related fields who wish to deepen their knowledge of backend systems to enhance their career prospects.",
+            },
+            {
+              group: "Entrepreneurs",
+              description:
+                "Business owners interested in understanding backend processes to better manage their web applications and software products.",
+            },
+            {
+              group: "Career Switchers",
+              description:
+                "Professionals from non-technical backgrounds looking to transition into backend development and the tech industry.",
+            },
+          ],
+        },
+        {
+          id: "3",
+          title: "Fullstack Development",
+          subtitle:
+            "Developing applications that combine user-centric interfaces and effective server logic for seamless, end-to-end experiences.",
+          monthone: 600,
+          monthtwo: 800,
+          monththree: 1000,
+          instructor: "Alice Johnson",
+          lessons: "6",
+          image: "course-img3.jpg",
+          durationone: "3 months",
+          durationtwo: "6 months",
+          durationthree: "9 months",
+          lessonone: "8",
+          lessontwo: "25",
+          lessonthree: "35",
+          description:
+            "This course offers a comprehensive introduction to fullstack development, covering both frontend and backend technologies. Students will learn to build complete web applications from the ground up, gaining expertise in server-side programming, database management, and user interface design. The curriculum includes real-world projects that enable students to create functional applications while mastering essential frameworks and tools.",
+          targetAudience: [
+            {
+              group: "Aspiring Fullstack Developers",
+              description:
+                "Individuals interested in becoming fullstack developers who wish to learn both frontend and backend skills.",
+            },
+            {
+              group: "Current Developers",
+              description:
+                "Frontend or backend developers looking to expand their skill set to include both sides of web development.",
+            },
+            {
+              group: "Tech Students",
+              description:
+                "Students pursuing degrees in computer science, software engineering, or related fields who want to acquire practical fullstack development skills.",
+            },
+            {
+              group: "Entrepreneurs",
+              description:
+                "Business owners wanting to understand the entire web development process to effectively manage their projects.",
+            },
+            {
+              group: "Career Changers",
+              description:
+                "Professionals from various backgrounds looking to transition into tech and gain a versatile skill set in web development.",
+            },
+            {
+              group: "Tech Enthusiasts",
+              description:
+                "Individuals passionate about technology who want to gain a holistic view of web development.",
+            },
+          ],
+        },
+        {
+          id: "4",
+          title: "Data Analysis",
+          subtitle:
+            "Analyzing data trends, creating visual insights, and leveraging predictive models to enable strategic decision-making.",
+          monthone: 6000,
+          monthtwo: 7000,
+          monththree: 85000,
+          durationone: "3 months",
+          durationtwo: "6 months",
+          durationthree: "9 months",
+          instructor: "John",
+          image: "course-img2.jpg",
+          description:
+            "This course provides a comprehensive overview of data analysis techniques, focusing on how to interpret complex data sets and derive actionable insights. Students will learn to use popular data analysis tools and programming languages such as Python and R, as well as data visualization tools like Tableau and Power BI. The curriculum includes hands-on projects that help students apply their analytical skills to real-world data scenarios.",
+          targetAudience: [
+            {
+              group: "Aspiring Data Analysts",
+              description:
+                "Individuals looking to start a career in data analysis and gain the skills needed to interpret and analyze data effectively.",
+            },
+            {
+              group: "Business Professionals",
+              description:
+                "Professionals in marketing, finance, and other fields who want to leverage data analysis to make informed business decisions.",
+            },
+            {
+              group: "Students in Related Fields",
+              description:
+                "Students studying statistics, data science, or business who want to enhance their understanding of data analysis techniques.",
+            },
+            {
+              group: "Entrepreneurs",
+              description:
+                "Business owners who wish to use data analysis to inform their business strategies and improve operational efficiency.",
+            },
+            {
+              group: "Career Changers",
+              description:
+                "Individuals from non-technical backgrounds looking to transition into data analysis roles in the tech industry.",
+            },
+            {
+              group: "Tech Enthusiasts",
+              description:
+                "Individuals interested in data science and analytics who want to learn how to work with data effectively.",
+            },
+          ],
+        },
+        {
+          id: "5",
+          title: "Artificial Intelligence",
+          subtitle:
+            "Applying machine learning and vision to develop technologies that mimic cognition, drive innovation, and assist decisions.",
+          monthone: 6000,
+          monthtwo: 7000,
+          monththree: 85000,
+          durationone: "3 months",
+          durationtwo: "6 months",
+          durationthree: "9 months",
+          instructor: "Chidera",
+          image: "course-img2.jpg",
+          description:
+            "This course covers foundational and advanced concepts in artificial intelligence, including machine learning, neural networks, and natural language processing. Students will gain experience with popular AI tools and frameworks, such as TensorFlow and PyTorch, and work on projects that simulate real-world applications of AI in fields like healthcare, finance, and robotics.",
+          targetAudience: [
+            {
+              group: "Aspiring AI Engineers",
+              description:
+                "Individuals aiming to build a career in AI, seeking skills in machine learning, neural networks, and data-driven decision-making.",
+            },
+            {
+              group: "Data Scientists and Analysts",
+              description:
+                "Professionals looking to extend their data skills into AI for predictive analytics and automated decision-making.",
+            },
+            {
+              group: "Software Developers",
+              description:
+                "Developers interested in integrating AI functionalities, such as recommendation engines or chatbots, into their applications.",
+            },
+            {
+              group: "Business Leaders",
+              description:
+                "Executives and managers who want to understand AI to make informed business decisions and lead AI initiatives.",
+            },
+            {
+              group: "Researchers and Academics",
+              description:
+                "Individuals in academia or research fields seeking knowledge in advanced AI concepts and methodologies for research applications.",
+            },
+            {
+              group: "Tech Enthusiasts",
+              description:
+                "Individuals passionate about AI who want to learn how to build intelligent systems and stay ahead in the tech industry.",
+            },
+          ],
+        },
+        {
+          id: "6",
+          title: "Cyber Security",
+          subtitle:
+            "Ensuring privacy, trust, and data protection with strategies and measures that safeguard sensitive information online.",
+          monthone: 6000,
+          monthtwo: 7000,
+          monththree: 85000,
+          durationone: "3 months",
+          durationtwo: "6 months",
+          durationthree: "9 months",
+          instructor: "Dr. Sarah Lee",
+          image: "course-img2.jpg",
+          description:
+            "This course offers an in-depth exploration of cybersecurity concepts, covering areas such as network security, ethical hacking, risk management, and threat mitigation. Students will work hands-on with tools like Wireshark, Kali Linux, and Metasploit to understand vulnerabilities, practice ethical hacking, and implement effective defense strategies. The curriculum includes real-world case studies, simulations, and projects designed to prepare students for roles in cybersecurity.",
+          targetAudience: [
+            {
+              group: "Aspiring Cybersecurity Specialists",
+              description:
+                "Individuals aiming to start a career in cybersecurity, including ethical hacking, threat analysis, and system protection.",
+            },
+            {
+              group: "IT Professionals",
+              description:
+                "System administrators and network engineers looking to strengthen their security skills and protect their organizations.",
+            },
+            {
+              group: "Business Executives",
+              description:
+                "Managers and executives interested in understanding cybersecurity risks to make informed decisions and protect their assets.",
+            },
+            {
+              group: "Students in Computer Science",
+              description:
+                "Students studying computer science, information systems, or related fields who want to specialize in cybersecurity.",
+            },
+            {
+              group: "Compliance Officers",
+              description:
+                "Professionals responsible for ensuring company policies align with cybersecurity laws, standards, and best practices.",
+            },
+            {
+              group: "Tech Enthusiasts",
+              description:
+                "Individuals with a passion for security who want to learn how to protect systems and data from potential threats.",
+            },
+          ],
+        },
+      ],
     };
   },
-  created() {
-    this.fetchCourses();
-  },
   methods: {
-    fetchCourses() {
-      AcademyService.getCourses().then((response) => {
-        this.courses = response.data;
-      });
-    },
     getStarted() {
-      this.$router.push({ name: "CourseList" }); // Change to your desired route
+      this.$router.push({ name: "CourseList" });
     },
   },
 };
 </script>
-
-<style>
-.about-container {
-  display: flex; /* Use Flexbox for layout */
-  align-items: center; /* Center items vertically */
-  justify-content: space-between; /* Space out items */
-  padding: 20px;
-  gap: 28px;
-}
-</style>
